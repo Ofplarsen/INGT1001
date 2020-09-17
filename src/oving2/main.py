6aea3424e514b206a3a1cd47df8de93126636f5b
Erlend Ryan <erlendryan@gmail.com>
2020-09-17 13:49:20 +0200

    oving2

--- src/oving2/main.py
+++ src/oving2/main.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env pybricks-micropython
-
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
-"""
-
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# Initialize the EV3 Brick.
+# Objects
 ev3 = EV3Brick()
+ultrasonic = UltrasonicSensor(Port.S2)
+touch = TouchSensor(Port.S1)
 
-# Initialize the motors.
+#Code
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-# Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=138)
 
+ev3.speaker.set_volume(50)
+ev3.speaker.beep()
+ev3.speaker.set_volume(10)
+
+
+while True:
+    if touch.pressed():
+        ev3.speaker.set_volume(50)
+        ev3.speaker.beep()
+        exit()
+    while ultrasonic.distance() > 100:
+        robot.drive(50,0)
+    robot.drive(-50,0)
+    robot.turn(20)
+    wait(10)
