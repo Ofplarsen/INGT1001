f785f453e9d53ee20a09b36470d7f355d0dde839
Vetle Malmin Skretting <vetlems@stud.ntnu.no>
2020-09-17 15:10:03 +0200

    Oving 2

--- src/oving2/main.py
+++ src/oving2/main.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env pybricks-micropython
-
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
-"""
-
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
@@ -23,6 +14,31 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
+# Initialize the sensors
+touch_sensor = TouchSensor(Port.S1)
+ultrasonic_sensor = UltrasonicSensor(Port.S2)
+
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=138)
 
+
+def wait_until_pressed(touch_sensor):
+    while not touch_sensor.pressed():
+        continue
+
+
+def drive_until_pressed(robot, touch_sensor, ultrasonic_sensor):
+    while not touch_sensor.pressed():
+        if ultrasonic_sensor.distance() > 150:
+            robot.drive(100, 0)
+        else:
+            robot.straight(-50)
+            robot.turn(30)
+    robot.stop()
+
+
+while True:
+    wait_until_pressed(touch_sensor)
+    ev3.speaker.say("Exercise 2")
+    drive_until_pressed(robot, touch_sensor, ultrasonic_sensor)
+    ev3.speaker.say("Exercise done")
