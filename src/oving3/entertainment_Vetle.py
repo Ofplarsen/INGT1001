db68e4de62a5eb0a514b5117e75e2e30a57cfc89
Vetle Malmin Skretting <vetlems@stud.ntnu.no>
2020-09-23 09:32:00 +0200

    Fikset Vetles underholdning

--- src/oving3/entertainment_Vetle.py
+++ src/oving3/entertainment_Vetle.py
@@ -5,8 +5,8 @@ def entertainment_vetle():
     small_motor.run(1000)
     notes = list()
     for i in range(2, 9):
-        for n in ['g', 'f', 'e', 'd', 'c', 'b', 'a']:
-            notes.append(n + str(n))
+        for n in ['G', 'F', 'E', 'D', 'C', 'B', 'A']:
+            notes.append(n + str(n) + '/8')
     ev3.speaker.set_volume(100)
     ev3.speaker.bee.play_notes(notes, tempo=120)
     small_motor.stop()
