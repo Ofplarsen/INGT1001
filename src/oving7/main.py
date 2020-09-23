76841b951da194779ec83a2090bad64e07069594
Erlend Matre <erlema@stud.ntnu.no>
2020-09-23 10:49:49 +0200

    twsrt

--- src/oving7/main.py
+++ src/oving7/main.py
@@ -5,10 +5,7 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
-import time 
-import random 
 # Objects
 ev3 = EV3Brick()
 
@@ -16,10 +13,6 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 small_motor = Motor(Port.D)
-
-# Initialize the sensors
-color_sensor = ColorSensor(Port.S1)
-ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=138)
@@ -29,7 +22,7 @@ ev3.speaker.set.volume(100)
 ev3.screen.clear()
 ev3.screen.print("Obama Chicken")
 ev3.speaker.say("Hello, I am President Obama.")
-ev3.play.notes(['C4','C4','G4.'], 90)
+ev3.play.notes(['C4_','C4_','G4.'], 90)
 ev3.small_motor.run(30)
 wait(2000)
 ev3.screen.clear()
