09f4f5a7e27b2b0ac23e183a02ad2496c051b12f
Erlend Ryan <erlendryan@gmail.com>
2020-09-23 09:09:21 +0200

    ay

--- src/oving3/entertainmentOlav.py
+++ src/oving3/entertainmentOlav.py
@@ -1,13 +1,2 @@
-import random
-
-
-def do_random_entertainment():
-    randomNum = random.randint(0, 3)
-    if (randomNum == 0):
-        entertainment1()
-    elif (randomNum == 1):
-        entertainment2()
-    elif (randomNum == 2):
-        entertainment3()
-    elif (randomNum == 3):
-        entertainment4()
+robot.turn(360)
+ev3.speaker.say("weeeeeeeeeeeee"
