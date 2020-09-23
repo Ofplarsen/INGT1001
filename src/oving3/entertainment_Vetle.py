24ee4cb2c465440715fef6504d169c5003165955
Vetle Malmin Skretting <vetlems@stud.ntnu.no>
2020-09-23 10:43:48 +0200

    Fullførte underholdning vetle

--- src/oving3/entertainment_Vetle.py
+++ src/oving3/entertainment_Vetle.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+# Initialize the EV3 Brick.
 ev3 = EV3Brick()
+
+# Initialize the motors
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
 small_motor = Motor(Port.D)
 
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=138)
+
 def entertainment_vetle():
-    small_motor.run(1000)
+    small_motor.run(100)
     notes = list()
-    for i in range(2, 9):
-        for n in ['G', 'F', 'E', 'D', 'C', 'B', 'A']:
-            notes.append(n + str(n) + '/8')
-    ev3.speaker.set_volume(100)
-    ev3.speaker.bee.play_notes(notes, tempo=120)
+    for i in range(2, 8):
+        for n in ["G", "F", "E", "D", "C", "B", "A"]:
+            notes.append(n + str(i) + "/8")
+    ev3.speaker.set_volume(1000)
+    ev3.speaker.play_notes(notes, tempo=120)
     small_motor.stop()
+
+entertainment_vetle()
