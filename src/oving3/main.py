b0fb3e812c44c1bc5079c3c91f90b29db578e76c
Erlend Ryan <erlendryan@gmail.com>
2020-09-23 08:49:19 +0200

    fargesensorshitvariablesyo

--- src/oving3/main.py
+++ src/oving3/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+from the color-sensor can have for the color to be defined as black
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -21,11 +22,20 @@ right_motor = Motor(Port.C)
 color_sensor = ColorSensor(Port.S1)
 ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
+# Global variables that defines the maximum RGB-values a sensor-reading
+RED = 50
+GREEN = 50
+BLUE = 50
+
+# Get the percentage of red, green, and blue color from the color-sensor
+(red, green, blue) = color_sensor.rgb()
+
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=138)
 
+
 class EV3_devices:
-    def __init__(self, robot = None, touch_sensor = None, color_sensor = None, infrared_sensor = None, ultrasonic_sensor = None, gyroscopic_sensor = None):
+    def __init__(self, robot=None, touch_sensor=None, color_sensor=None, infrared_sensor=None, ultrasonic_sensor=None, gyroscopic_sensor=None):
         self.robot = robot
         self.touch_sensor = touch_sensor
         self.color_sensor = color_sensor
@@ -33,29 +43,40 @@ class EV3_devices:
         self.ultrasonic_sensor = ultrasonic_sensor
         self.gyroscopic_sensor = gyroscopic_sensor
 
-ev3_devices = EV3_devices(robot=robot, color_sensor=color_sensor, ultrasonic_sensor=ultrasonic_sensor)
+
+ev3_devices = EV3_devices(
+    robot=robot, color_sensor=color_sensor, ultrasonic_sensor=ultrasonic_sensor)
+
 
 def entertainment1(ev3_devices):
     pass
 
+
 def entertainment2(ev3_devices):
     pass
+
 
 def entertainment3(ev3_devices):
     pass
 
+
 def entertainment4(ev3_devices):
     pass
 
+
 def do_random_entertainment(ev3_devices):
-    entertainments = [entertainment1, entertainment2, entertainment3, entertainment4]
+    entertainments = [entertainment1, entertainment2,
+                      entertainment3, entertainment4]
     random.choice(entertainments)(ev3_devices)
+
 
 def play_sound():
     pass
 
+
 def follow_track():
     pass
+
 
 while True:
     current_time = time.time()
